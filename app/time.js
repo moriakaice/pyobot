145700e957bba7a98f90c2f88ed3324caf48c15e
Mori <moriakaice@gmail.com>
2017-08-09 19:39:04 +0100

    Extend time functions

--- app/time.js
+++ app/time.js
@@ -1,4 +1,11 @@
 module.exports = {
+  getDate: (timecode) => {
+    const date = timecode ? new Date(timecode) : new Date()
+    const fullYear = date.getFullYear()
+    const month = '0' + (date.getMonth() + 1)
+    const day = '0' + date.getDate()
+    return fullYear + '-' + month.substr(-2) + '-' + day.substr(-2)
+  },
   getTime: (timecode) => {
     const date = timecode ? new Date(timecode) : new Date()
     const hours = '0' + date.getHours()
@@ -19,5 +26,33 @@ module.exports = {
     const secondString = '0' + seconds % 60
 
     return minuteString.substr(-2) + ':' + secondString.substr(-2)
+  },
+  timeToFullString: (seconds) => {
+    if (seconds < 0) {
+      seconds = 0
+    }
+
+    const timeArray = []
+
+    if (seconds >= 86400) {
+      timeArray.push(Math.floor(seconds / 86400) + ' d')
+      seconds = seconds % 86400
+    }
+
+    if (seconds >= 3600) {
+      timeArray.push(Math.floor(seconds / 3600) + ' h')
+      seconds = seconds % 3600
+    }
+
+    if (seconds >= 60) {
+      timeArray.push(Math.floor(seconds / 60) + ' m')
+      seconds = seconds % 60
+    }
+
+    if (seconds > 0) {
+      timeArray.push(seconds + ' s')
+    }
+
+    return timeArray.length ? timeArray.join(' ') : ''
   },
 }
