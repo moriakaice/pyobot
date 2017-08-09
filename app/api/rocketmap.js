a3f533a2a1b53d418607ff98920c9dca40778c87
Mori <moriakaice@gmail.com>
2017-08-09 19:04:13 +0100

    Fix RM notifications format

--- app/api/rocketmap.js
+++ app/api/rocketmap.js
@@ -19,28 +19,35 @@ const rocketMap = {
         try {
           body = JSON.parse(body)
 
-          if (body.type === "pokemon" && body.message) {
-            // Translate RM Pokemon to LPM Pokemon
-            const newPokemon = {
-              pokemon_id: body.message.pokemon_id.toString(),
-              lat: body.message.latitude.toString(),
-              lng: body.message.longitude.toString(),
-              despawn: Math.floor(body.message.disappear_time / 1000).toString(),
-              disguise: '0',
-              costume: '0',
-              shiny: '0',
-              attack: body.message.individual_attack != null ? body.message.individual_attack.toString() : '-1',
-              defence: body.message.individual_defense != null ? body.message.individual_defense.toString() : '-1',
-              stamina: body.message.individual_stamina != null ? body.message.individual_stamina.toString() : '-1',
-              move1: body.message.move_1 != null ? body.message.move_1.toString() : '-1',
-              move2: body.message.move_2 != null ? body.message.move_2.toString() : '-1',
-              gender: body.message.gender.toString(),
-              form: body.message.form != null ? body.message.form.toString() : '0',
-              cp: body.message.cp != null ? body.message.cp.toString() : '-1',
-            }
-            pokemonUtil.parsePokemon(newPokemon)
+          if (Array.isArray(body)) {
+            const pokemons = []
+            body.forEach(message => {
+              if (message.type === "pokemon" && message.message) {
+                // Translate RM Pokemon to LPM Pokemon
+                const newPokemon = {
+                  pokemon_id: message.message.pokemon_id.toString(),
+                  lat: message.message.latitude.toString(),
+                  lng: message.message.longitude.toString(),
+                  despawn: message.message.disappear_time.toString(),
+                  disguise: '0',
+                  costume: '0',
+                  shiny: '0',
+                  attack: message.message.individual_attack != null ? message.message.individual_attack.toString() : '-1',
+                  defence: message.message.individual_defense != null ? message.message.individual_defense.toString() : '-1',
+                  stamina: message.message.individual_stamina != null ? message.message.individual_stamina.toString() : '-1',
+                  move1: message.message.move_1 != null ? message.message.move_1.toString() : '-1',
+                  move2: message.message.move_2 != null ? message.message.move_2.toString() : '-1',
+                  gender: message.message.gender.toString(),
+                  form: message.message.form != null ? message.message.form.toString() : '0',
+                  cp: message.message.cp != null ? message.message.cp.toString() : '-1',
+                }
+
+                pokemons.push(newPokemon)
+              }
+            })
+            pokemonUtil.parsePokemon(pokemons)
             res.writeHead(200)
-            res.end(JSON.stringify(newPokemon))
+            res.end()
           } else {
             res.writeHead(200)
             res.end('The only supported type is "pokemon"')
